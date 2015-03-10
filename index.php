c813e03f11bd25faeb4d361435a08c8b12922417
Jorge Olivas <olivas.daw@gmail.com>
2015-03-10 18:06:15 +0100

    primera subida

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head><title>Jorge Olivas</title></head>
+<link rel="stylesheet" href="css/main.css" type="text/css" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<body>
+	<div id="barr" class="headerBar fixed">
+		
+			<div class="hB">
+				Home
+			</div>
+			<div class="hB">
+				Galeria
+			</div>
+			<div class="hB">
+				Faqs
+			</div>
+		
+	</div>
+</body>
+</html>
